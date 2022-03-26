2a1a70ad591ea3599d1a778d866c99e7c13b9a63
Aleksey Eliseev <alekseyeliseev@ya.ru>
2022-03-26 18:30:17 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import asyncio
+
+from pywebio import start_server
+from pywebio.input import *
+from pywebio.output import *
+
+from models import Website, Crawler
+
+
+def app():
+
+    put_markdown(
+        "# Краулер и парсер веб-ресурсов\nСервис предоставляет возможность обхода страниц разделов новостных веб-ресурсов с возможностью парсинга их содержимого.")
+
+    info = input_group("Параметры поиска", [
+        input('Имя набора параметров', name='name', type=TEXT, help_text='Пример: Новые известия'),
+        input('Стартовый адрес', name='url', type=URL, required=True, help_text='Пример: https://newizv.ru'),
+        input('Паттерн для поиска в разделах', name='targetPattern', type=TEXT, required=True, help_text='Пример: ^(/news/)'),
+        input('Тег для выделения заголовка',
+              name='titleTag', type=TEXT, required=True, help_text='Пример: div.title'),
+        input('Тег для выделения текста статьи',
+              name='bodyTag', type=TEXT, required=True, help_text='Пример: div.content-blocks')
+    ])
+
+    site = Website(info['name'], info['url'], info['targetPattern'],
+                   False, info['titleTag'], info['bodyTag'])
+    crawl(site)
+
+
+def crawl(site):
+    crawler = Crawler(site)
+    with put_loading(shape='border', color='info').style('width:2rem; height:2rem'):
+        results = crawler.crawl()
+    print_results(results)
+
+
+def print_results(results):
+    put_markdown('## Результаты поиска')
+    for result in results:
+        put_markdown(f'[{result.url}]({result.url})')
+        put_markdown(f'**{result.title}**')
+        put_text(result.body).style('paddin-bottom: 10px')
+
+
+if __name__ == '__main__':
+    start_server(app, debug=True, port=80)
---
+++ models.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+class Website:
+
+    def __init__(self, name, url, targetPattern, absoluteUrl, titleTag, bodyTag):
+        self.name = name
+        self.url = url
+        self.targetPattern = targetPattern
+        self.absoluteUrl = absoluteUrl
+        self.titleTag = titleTag
+        self.bodyTag = bodyTag
+
+
+class Content:
+
+    def __init__(self, url, title, body):
+        self.url = url
+        self.title = title
+        self.body = body
+
+
+class Crawler:
+    def __init__(self, site):
+        self.site = site
+        self.visited = []
+        self.results = []
+
+    def getPage(self, url):
+        try:
+            req = requests.get(url)
+        except requests.exceptions.RequestException:
+            return None
+        return BeautifulSoup(req.text, 'html.parser')
+
+    def safeGet(self, pageObj, selector):
+        selectedElems = pageObj.select(selector)
+        '''
+        if selectedElems is not None and len(selectedElems) > 0:
+            return '\n'.join([elem.get_text() for elem in selectedElems])
+        return ''
+        '''
+        return selectedElems[0].get_text()
+
+    def parse(self, url):
+        bs = self.getPage(url)
+        if bs is not None:
+            title = self.safeGet(bs, self.site.titleTag).rstrip() #убирает последний пробел в заголовке
+            body = self.safeGet(bs, self.site.bodyTag)
+            if title != '' and body != '':
+                content = Content(url, title, body)
+                self.results.append(content)
+
+    def crawl(self):
+        """
+        Получение страниц, начиная со стартовой
+        """
+        bs = self.getPage(self.site.url)
+        targetPages = bs.findAll('a', href=re.compile(self.site.targetPattern))
+        for targetPage in targetPages:
+            targetPage = targetPage.attrs['href']
+            if targetPage not in self.visited:
+                self.visited.append(targetPage)
+                if not self.site.absoluteUrl:
+                    targetPage = '{}{}'.format(self.site.url, targetPage)
+                self.parse(targetPage)
+        return self.results
